64ece3dee1d153d78addf9b56a20cf4304ccc5e3
LUKASRIB15 <lucas.ribeiro06@aluno.ifce.edu.br>
2025-02-07 11:29:54 -0300

    :wrench: Testing openai lib

--- src/infra/http/controllers/design-description.ts
+++ src/infra/http/controllers/design-description.ts
@@ -1,5 +1,28 @@
 import { FastifyReply, FastifyRequest } from "fastify";
+import { z } from "zod";
+import { openai } from "../../lib/openai";
+
 
 export async function designDescription(request: FastifyRequest, reply: FastifyReply){
-  return 'Hello world'
+  const designDescriptionBodySchema = z.object({
+    prompt: z.string(),
+  })
+
+  const { prompt } = designDescriptionBodySchema.parse(request.body)
+
+  const response = await openai.chat.completions.create({
+    model: 'gpt-3.5-turbo',
+    messages: [
+      {
+        role: "system",
+        content: "You are designer and should offer a description analysis of the user prompt and the photo sent"
+      },
+      {
+        role: "user",
+        content: prompt
+      }
+    ]
+  })
+
+  return reply.status(200).send(response.choices[0].message.content);
 }
